7ec2696188f85bf23ddddfc13731d0817e422110
erpao <lizaizai2022@163.com>
2022-06-17 09:25:53 +0800

    first commit

---
+++ src/main/java/com/netopstec/learnjenkins/controller/Hello.java
@@ -0,0 +1,24 @@
+package com.netopstec.learnjenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zhenye 2018/3/14
+ */
+@RestController
+public class Hello {
+
+    @GetMapping("/")
+    public String sayHello() {
+        System.out.println("项目已经在jenkins上部署成功，测试git push origin master");
+        return "Hello World";
+    }
+
+    @GetMapping("/jenkins")
+    public String sayToJenkins() {
+        System.out.println("尝试提交代码到github上后，jenkins自动构建");
+        System.out.println("再次尝试提交代码到github，检测jenkins是否会自动构建");
+        return "The first time to learn jenkins";
+    }
+}
